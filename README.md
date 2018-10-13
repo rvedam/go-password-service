73aab2f1e27f34209ef388040ec5299abf83f1e0
Ram Vedam <rvedam@gmail.com>
2018-10-12 22:37:32 -0400

    Added CLI flags for running on different ports

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 A simple web server implementing a password hashing service
 
 # Build instructions
+1. First clone the project (you can use `go get` for this):
+```bash
+go get https://github.com/rvedam/go-password-service
+```
+2. To build just simply run `go build`:
+```bash
+go build
+```
--- main.go
+++ main.go
@@ -2,17 +2,29 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/rvedam/go-password-service/server"
 )
 
 func main() {
+	portPtr := flag.Int("port", 8080, "specify a port number to listen on")
+	helpPtr := flag.Bool("help", false, "generate help")
+
+	flag.Parse()
+
+	if *helpPtr == true {
+		flag.PrintDefaults()
+		return
+	}
+
 	stop := make(chan bool)
-	srv := &http.Server{Addr: ":8080", Handler: server.NewServer(stop)}
+	srv := &http.Server{Addr: ":" + strconv.Itoa(*portPtr), Handler: server.NewServer(stop)}
 	go func() {
 		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
 			// Error starting or closing listener:
@@ -20,6 +32,7 @@ func main() {
 		}
 	}()
 
+	fmt.Println("Server listening on 0.0.0.0:" + strconv.Itoa(*portPtr))
 	<-stop
 	log.Println("Shutting down server...")
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
